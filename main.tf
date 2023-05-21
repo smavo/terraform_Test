5d79445af9228da94ca2f1d569ec5748d91755c2
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-05-21 15:10:04 -0500

    Creando 2 instancias con subredes independientes

--- main.tf
+++ main.tf
@@ -3,14 +3,44 @@ provider "aws" {
   region = var.region
 }
 
-# Define la instancia EC2 
-resource "aws_instance" "servidor_smavo"{
+# Data Source del ID dela VPC por defecto
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Data Source obtiene el ID del AZ 1
+data "aws_subnet" "az_c"{
+  availability_zone = "us-east-1c"
+}
+
+# Data Source obtiene el ID del AZ 2
+data "aws_subnet" "az_b"{
+  availability_zone = "us-east-1b"
+}
+
+# Instancia EC2 - Primera Instancia
+resource "aws_instance" "servidor_smavo_I"{
     ami = var.ami
     instance_type = "t2.micro"
+    subnet_id = data.aws_subnet.az_b.id
     vpc_security_group_ids = [ aws_security_group.mi_sg.id ]
     tags = {
-      Name = "Instancia EC2 en terraform"
-      Enviroment = "Desarrollo"
+      Name = "Instancia EC2 en terraform I"
+      Enviroment = "Desarrollo A"
+      Manager = "Terraform"
+    }
+    user_data = "${file("user-data-apache.sh")}"
+}
+
+# Instancia EC2 - Segunda Instancia
+resource "aws_instance" "servidor_smavo_II"{
+    ami = var.ami
+    instance_type = "t2.micro"
+    subnet_id = data.aws_subnet.az_c.id
+    vpc_security_group_ids = [ aws_security_group.mi_sg.id ]
+    tags = {
+      Name = "Instancia EC2 en terraform II"
+      Enviroment = "Desarrollo B"
       Manager = "Terraform"
     }
     user_data = "${file("user-data-apache.sh")}"
@@ -19,7 +49,7 @@ resource "aws_instance" "servidor_smavo"{
 # Define un grupo de seguridad con acceso al puerto 8080
 resource "aws_security_group" "mi_sg" {            
   name = "primer_server_sg"
-
+  vpc_id = data.aws_vpc.default.id
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Acceso al puerto 8080 desde el exterior"
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,15 @@
-output "dns_publica" {
+output "dns_publica_server_1" {
     description = "DNS Publica del servidor"
-    value = "http://${aws_instance.servidor_smavo}:8080"
+    value = aws_instance.servidor_smavo_I
 }
 
-output "ipv4_servidor" {
-    description = "IPv4 de nuestro servidor"
-    value = aws_instance.servidor_smavo.public_ip
+output "dns_publica_server_2" {
+    description = "DNS Publica del servidor"
+    value = aws_instance.servidor_smavo_II
 }
+
+
+/* output "ipv4_servidor_1" {
+    description = "IPv4 de nuestro servidor"
+    value = aws_instance.servidor_smavo_I
+} */
