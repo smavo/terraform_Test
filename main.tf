a4f7a933dec9a34a5a7edbe93547d77bea384bac
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-05-20 19:33:27 -0500

    Segundo ejemplo de creacion de infraestructura con Terraform + Variables de ambiente

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Define el provider de aws
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
   access_key = "AKIAUXW453BIR5JRYL64"
   secret_key = "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"
 }
 
 # Define la instancia EC2 
 resource "aws_instance" "servidor_smavo"{
-    ami = "ami-053b0d53c279acc90"
+    ami = var.ami
     instance_type = "t2.micro"
         tags = {
         Name = "Instancia de prueba en test terraform"
