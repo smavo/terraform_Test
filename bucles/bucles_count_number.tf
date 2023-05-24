be74076ade3060c079d53a6770c1500912e99397
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-05-23 21:09:32 -0500

    Add ejemplo de bucle count number + Output

---
+++ bucles/bucles_count_number.tf
@@ -0,0 +1,30 @@
+# -------------------------
+# Define el provider de AWS
+# -------------------------
+provider "aws" {
+  region = "eu-west-1"
+} 
+
+variable "usuarios_2" {
+  description = "Nombre de usuarios IAM"
+  type = number
+  default = 3
+}
+
+# ---------------------------------
+# Crea un <var.usuarios> de IAM
+# ---------------------------------
+resource "aws_iam_user" "usuarios_2" {
+  count = var.usuarios_2
+  
+  name = "user-number.${count.index}"
+}
+
+# Outputs de Salida
+output "arn_usuarios_2" {
+  value = aws_iam_user.usuarios_2[2].arn
+}
+
+output "arn_todos_usuarios" {
+  value = [for usuario in aws_iam_user.usuarios_2 : usuario.arn]
+}
